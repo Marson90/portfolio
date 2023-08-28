5c31b52e7cdb4f5cf084c85a80bca87b76c9ec65
Andrew Marson <63836571+Marson90@users.noreply.github.com>
2023-08-28 03:58:16 -0400

    Add files via upload

---
+++ pythonproj1.py
@@ -0,0 +1,23 @@
+import random
+import secrets
+import string
+
+#acquire characters
+letters = string.ascii_letters
+digits = string.digits
+specialchar = string.punctuation 
+
+alphabet = letters + digits + specialchar 
+
+#setting password length
+pwd_length = 12
+#generating password with all the characters in random order meeting a constraint
+while True:
+    pwd = ''
+    for i in length(pwd_length):
+        pwd += ''.join(secrets.choice(alphabet))
+
+    if(any(char in specialchars for char in pwd)and 
+       sum(char in digits for char in pwd)>=2):
+        break
+print(pwd)
